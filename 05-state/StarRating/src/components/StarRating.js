2008ae2cf58d5d8ec58149f75e73e5a0febb9daf
范轶洁 <fanyjb@digitalchina.com>
2020-02-08 22:29:38 +0800

    re-org

--- 05-state/StarRating/src/components/StarRating.js
+++ 05-state/StarRating/src/components/StarRating.js
@@ -4,11 +4,15 @@ import Star from "./Star";
 const createArray = length => [...Array(length)];
 
 export default function StarRating({ totalStars = 5 }) {
-  const [selectedStars] = useState(3);
+  const [selectedStars, setSelectedStars] = useState(0);
   return (
     <>
       {createArray(totalStars).map((n, i) => (
-        <Star key={i} selected={selectedStars > i} />
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
       ))}
       <p>
         {selectedStars} of {totalStars} stars
--- 05-state/src/components/Star.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { FaStar } from "react-icons/fa";
-
-const Star = ({ selected = false }) => (
-  <FaStar color={selected ? "red" : "grey"} />
-);
-
-export default Star;
