94bd50092c846aed014af9ca7f4eae747f5c327a
范轶洁 <fanyjb@digitalchina.com>
2020-02-08 21:03:23 +0800

    upgrade with webpack 4

--- 05-state/webpack.config.js
+++ 05-state/webpack.config.js
@@ -1,13 +1,26 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "State Management",
+      template: "index.html"
+    })
+  ],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
   },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
-  },
-  devtool: "#source-map" // Add this option for source mapping
+  }
 };
