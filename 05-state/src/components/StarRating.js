0440bd4004f122d0be4d54a4bd1213c87d8a0767
范轶洁 <fanyjb@digitalchina.com>
2020-02-08 21:21:16 +0800

    StarRating

--- 05-state/src/components/StarRating.js
+++ 05-state/src/components/StarRating.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Star from "./Star";
 
 const createArray = length => [...Array(length)];
 
 export default function StarRating({ totalStars = 5 }) {
-  return createArray(totalStars).map((n, i) => <Star key={i} />);
+  const [selectedStars] = useState(3);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star key={i} selected={selectedStars > i} />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
 }
