2a7d96027aab8246956bd94db6dc1449d670fb27
范轶洁 <fanyjb@digitalchina.com>
2020-02-09 11:59:56 +0800

    custom hook

--- 05-state/Form/src/components/App.js
+++ 05-state/Form/src/components/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import AddColorForm from "./AddColorForm";
+import ControlledAddColorForm from "./ControlledAddColorForm";
+import CustomHooksAddColorForm from "./CustomHooksAddColorForm";
 export default () => (
-  <AddColorForm
-    onNewColor={(title, color) => {
-      console.log(title, color);
-    }}
-  />
+  <>
+    <AddColorForm
+      onNewColor={(title, color) => {
+        console.log(title, color);
+      }}
+    />
+    <ControlledAddColorForm
+      onNewColor={(title, color) => {
+        console.log(title, color);
+      }}
+    />
+    <CustomHooksAddColorForm
+      onNewColor={(title, color) => {
+        console.log(title, color);
+      }}
+    />
+  </>
 );
