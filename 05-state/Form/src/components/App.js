b8957156b19effbd767fa323929be123eb128cb6
范轶洁 <fanyjb@digitalchina.com>
2020-02-09 10:21:02 +0800

    form setup

---
+++ 05-state/Form/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>Hello World!</h1>
+    <ul>
+      <li>New York</li>
+      <li>Los Angel</li>
+      <li>Tokyo</li>
+    </ul>
+  </div>
+);
