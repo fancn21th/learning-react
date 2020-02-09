a171780c9694061efbda613e9ea87d65d5f99149
范轶洁 <fanyjb@digitalchina.com>
2020-02-09 21:35:52 +0800

    use effect

--- 06-useEffect/DependencyArray/src/components/App.js
+++ 06-useEffect/DependencyArray/src/components/App.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function App() {
-  return <div>Hello World!</div>;
+function App() {
+  const [val, set] = useState("");
+  const [phrase, setPhrase] = useState("example phrase");
+
+  const createPhrase = () => {
+    setPhrase(val);
+    set("");
+  };
+
+  useEffect(() => {
+    console.log(`typing "${val}"`);
+  }, [val]);
+
+  useEffect(() => {
+    console.log(`saved phrase: "${phrase}"`);
+  }, [phrase]);
+
+  return (
+    <>
+      <label>Favorite phrase:</label>
+      <input
+        value={val}
+        placeholder={phrase}
+        onChange={e => set(e.target.value)}
+      />
+      <button onClick={createPhrase}>send</button>
+    </>
+  );
 }
+
+export default App;
