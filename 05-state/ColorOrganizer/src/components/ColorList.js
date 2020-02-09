54fa9f7d2b46c9ff5e9854b48ed1c9b3e109f986
范轶洁 <fanyjb@digitalchina.com>
2020-02-09 09:22:44 +0800

    rating

--- 05-state/ColorOrganizer/src/components/ColorList.js
+++ 05-state/ColorOrganizer/src/components/ColorList.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Color from "./Color";
 
-export default function ColorList({ colors = [] }) {
+export default function ColorList({
+  colors = [],
+  onRemoveColor = f => f,
+  onRateColor = f => f
+}) {
   return (
-    <div>
+    <div className="color-list">
       {colors.length === 0 ? (
         <p>No Colors Listed. (Add a Color)</p>
       ) : (
-        colors.map(color => <Color key={color.id} {...color} />)
+        colors.map(color => (
+          <Color
+            key={color.id}
+            {...color}
+            onRemove={onRemoveColor}
+            onRate={onRateColor}
+          />
+        ))
       )}
     </div>
   );
--- 05-state/ColorOrganizer/src/components/StarRating.js
+++ 05-state/ColorOrganizer/src/components/StarRating.js
@@ -3,7 +3,11 @@ import Star from "./Star";
 
 const createArray = length => [...Array(length)];
 
-export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
+export default function StarRating({
+  totalStars = 5,
+  selectedStars = 0,
+  onRate = f => f
+}) {
   return (
     <>
       {createArray(totalStars).map((n, i) => (
