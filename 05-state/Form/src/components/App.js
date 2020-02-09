e3831f64112c55d0b55fb1edcfc9dddadbdd7a1b
范轶洁 <fanyjb@digitalchina.com>
2020-02-09 12:33:10 +0800

    form with adding color

--- 05-state/Form/src/components/App.js
+++ 05-state/Form/src/components/App.js
@@ -1,23 +1,52 @@
-import React from "react";
-import AddColorForm from "./AddColorForm";
-import ControlledAddColorForm from "./ControlledAddColorForm";
-import CustomHooksAddColorForm from "./CustomHooksAddColorForm";
-export default () => (
-  <>
-    <AddColorForm
-      onNewColor={(title, color) => {
-        console.log(title, color);
-      }}
-    />
-    <ControlledAddColorForm
-      onNewColor={(title, color) => {
-        console.log(title, color);
-      }}
-    />
-    <CustomHooksAddColorForm
-      onNewColor={(title, color) => {
-        console.log(title, color);
-      }}
-    />
-  </>
-);
+import React, { useState } from "react";
+import AddColorForm from "./form/AddColorForm";
+import ControlledAddColorForm from "./form/ControlledAddColorForm";
+import CustomHooksAddColorForm from "./form/CustomHooksAddColorForm";
+import colorData from "./ui/color-data.json";
+import ColorList from "./ui/ColorList.js";
+import { v4 } from "uuid";
+
+export default () => {
+  const [colors, setColors] = useState(colorData);
+  return (
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => {
+          console.log(title, color);
+        }}
+      />
+      <ControlledAddColorForm
+        onNewColor={(title, color) => {
+          console.log(title, color);
+        }}
+      />
+      <CustomHooksAddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [
+            ...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              color
+            }
+          ];
+          setColors(newColors);
+        }}
+      />
+      <ColorList
+        colors={colors}
+        onRemoveColor={id => {
+          const newColors = colors.filter(color => color.id !== id);
+          setColors(newColors);
+        }}
+        onRateColor={(id, rating) => {
+          const newColors = colors.map(color =>
+            color.id === id ? { ...color, rating } : color
+          );
+          setColors(newColors);
+        }}
+      />
+    </>
+  );
+};
---
+++ 05-state/Form/src/components/ui/StarRating.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Star from "./Star";
+
+const createArray = length => [...Array(length)];
+
+export default function StarRating({
+  totalStars = 5,
+  selectedStars = 0,
+  onRate = f => f
+}) {
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => onRate(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
+}
